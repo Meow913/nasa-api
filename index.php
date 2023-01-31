eda066cdc16664445e541c881ba05475c35a31d5
Meow913 <tatyanko@bk.ru>
2023-01-31 12:54:01 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="HandheldFriendly" content="true"/>
+
+    <title>Nasa API</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300&display=swap" rel="stylesheet">
+
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+    <script type="text/javascript">
+
+        $(function() {
+
+            $(window).scroll(function() {
+
+                if($(this).scrollTop() != 0) {
+
+                    $('#toTop').fadeIn();
+
+                } else {
+
+                    $('#toTop').fadeOut();
+
+                }
+
+            });
+
+            $('#toTop').click(function() {
+
+                $('body,html').animate({scrollTop:0},800);
+
+            });
+            // $('#toTop').touchstart(function() {
+            //
+            //     $('body,html').animate({scrollTop:0},800);
+            //
+            // });
+
+
+        });
+
+    </script>
+</head>
+<body>
+<!-- <audio src="morning.mp3" autoplay loop></audio> -->
+<div class="main-page-div">
+    <div class="header-main-div">
+        <div class="left-subheader-div">
+            <img src="nasa.svg.png"  alt="NASA API">
+        </div>
+        <div class="right-subheader-div">
+
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a class="link-APOD">Astronomy Picture of the Day</a></li>
+                        <li><a class="video-link">Videos</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <div class="central-main-div">
+        <div class="top-message-div">
+            <h1>Hello, earthlings!</h1>
+            <p>Here you can find photos of our planet, the other planets and galaxies too!</p>
+        </div>
+        <div class="bottom-search-div">
+            <form class="search-form">
+                <input type="text" name="search-input" placeholder="Search for an image" class="search-input-class">
+                <button type="submit" id="search-id" class="test">SEARCH</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="video-back-div">
+        <video class="back-video" autoplay loop muted >
+            <source src="video.mp4" type="video/webm">
+            Your browser does not support the video tag.
+        </video>
+
+    </div>
+
+
+    <div id="toTop"> ^ Наверх </div>
+
+</div>
+
+<script src="script.js"></script>
+</body>
+</html>
+
---
+++ script.js
@@ -0,0 +1,330 @@
+let key = "HFRtdBD3jyBtyccF7xLF1bGQKFfjSM9FNEfRKgyg";
+let addressAPOD = "https://api.nasa.gov/planetary/apod?api_key=HFRtdBD3jyBtyccF7xLF1bGQKFfjSM9FNEfRKgyg";
+let addresOfPics = "https://images-api.nasa.gov/search?q=";
+let closeBtn = document.getElementById('close-id');
+let allLinks = document.querySelector('.right-subheader-div');
+let centralDiv = document.querySelector('.central-main-div');
+
+
+window.onload = function(){
+    if (document.documentElement.clientWidth > 760 && document.querySelector('.links-menu') === null)
+    {
+        createMenuLinksFunc();
+        runLinkAPOD();
+    }
+    if (document.documentElement.clientWidth < 760 && document.querySelector('.menu-btn') === null)
+    {
+        createBurgerBtnFunc ();
+        runLinkAPOD();
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    if (document.documentElement.clientWidth < 760 && document.querySelector('.links-menu') !== null)
+    {
+        document.querySelector('.links-menu').remove();
+        runLinkAPOD();
+    }
+    if(document.documentElement.clientWidth > 760 && document.querySelector('.links-menu') === null)
+        {
+            createMenuLinksFunc();
+            runLinkAPOD();
+    }
+    if (document.documentElement.clientWidth > 760 && document.querySelector('.menu-btn') !== null)
+    {
+        document.querySelector('.menu-btn').remove();
+    }
+    if (document.documentElement.clientWidth < 760 && document.querySelector('.menu-btn') === null)
+    {
+        createBurgerBtnFunc ();
+    }
+});
+
+document.querySelector('.bottom-search-div').addEventListener('click', function(event) {
+    if (event.target.matches('button')) {
+        event.preventDefault();
+        buttonClickEffect();
+        createSearchResultFunc();
+        connectApiPicsVideo();
+    }
+});
+
+document.querySelector('.bottom-search-div').addEventListener('touchstart', function(event) {
+    if (event.target.matches('button')) {
+        event.preventDefault();
+        buttonClickEffect();
+        createSearchResultFunc();
+        connectApiPicsVideo();
+    }
+});
+
+let linkHome = document.querySelector('.link-home');
+linkHome.addEventListener('click', () => {
+    console.log('home');
+    document.getElementsByName('search-input')[0].placeholder='search for images';
+    document.getElementsByName('search-input')[0].value='';
+    document.getElementsByName('search-input')[0].dataset.typeOfSearch = 'image';
+})
+let linkVideo = document.querySelector('.video-link');
+linkVideo.addEventListener('click', () => {
+    console.log('VIDEO');
+    document.getElementsByName('search-input')[0].placeholder='search for videos';
+    document.getElementsByName('search-input')[0].value='';
+    document.getElementsByName('search-input')[0].dataset.typeOfSearch = 'video';
+})
+
+function createMenuLinksFunc()
+{
+    let divMenuLinks = document.createElement('div');
+    divMenuLinks.className = 'links-menu';
+    divMenuLinks.innerHTML = '<a class="link-home">Home</a>\n' +
+        '<a  class ="link-APOD">Astronomy Picture of the Day</a>\n' +
+        '<a class="video-link">Videos</a>';
+    document.querySelector('.right-subheader-div').prepend(divMenuLinks);
+    document.getElementsByName('search-input')[0].dataset.typeOfSearch = 'image';
+
+    let linkHome = document.querySelector('.link-home');
+    linkHome.addEventListener('click', () => {
+        console.log('home');
+        document.getElementsByName('search-input')[0].placeholder='search for images';
+        document.getElementsByName('search-input')[0].value='';
+        document.getElementsByName('search-input')[0].dataset.typeOfSearch = 'image';
+    })
+    let linkVideo = document.querySelector('.video-link');
+    linkVideo.addEventListener('click', () => {
+        console.log('VIDEO');
+        document.getElementsByName('search-input')[0].placeholder='search for videos';
+        document.getElementsByName('search-input')[0].value='';
+        document.getElementsByName('search-input')[0].dataset.typeOfSearch = 'video';
+    })
+}
+
+function createBurgerBtnFunc()
+{
+    let menuBtnVar = document.createElement('div');
+    menuBtnVar.className = 'menu-btn';
+    menuBtnVar.innerHTML = '<span></span>' +
+        '<span></span>' +
+        '<span></span>';
+    document.querySelector('.right-subheader-div').append(menuBtnVar);
+
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.menu');
+    menuBtn.addEventListener('click', function(){
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+    })
+}
+
+function createSearchResultFunc()
+{
+    let divResult = document.createElement('div');
+    divResult.className = 'search-result';
+    document.querySelector('.main-page-div').append(divResult);
+}
+function createAPODFunc()
+{
+    let divAPOD = document.createElement('div');
+    divAPOD.className = 'modal-APOD-main';
+    divAPOD.innerHTML = '<div class="img-div-APOD"></div>' +
+        '<div class="content-div-APOD">\n' +
+        '            <div class="title-div-APOD"></div>\n' +
+        '            <div class="date-div-APOD"></div>\n' +
+        '            <div class="text-div-APOD"></div>\n' +
+        '        </div>\n';
+    document.querySelector('.main-page-div').append(divAPOD);
+}
+
+function runLinkAPOD()
+{
+    let linkAPOD = document.querySelector('.link-APOD');
+    linkAPOD.addEventListener('click', () => {
+        createAPODFunc();
+        connectApiAPOD();
+        setTimeout(function timeFunc() {
+            allLinks.classList.add('hidden-class');
+            centralDiv.classList.add('hidden-class');
+            if(document.querySelector('.search-result')){
+                document.querySelector('.search-result').classList.add('hidden-class');
+            }
+        }, 300);
+    });
+
+}
+
+
+
+
+
+
+
+
+function buttonClickEffect() {
+    let searchBtn = document.getElementById('search-id');
+    searchBtn.classList.add('active-btn');
+    setTimeout(function timeFunc() {
+        searchBtn.classList.remove('active-btn');
+    }, 300);
+}
+
+function buttonClickEffectClose()
+{
+    let closeBtn = document.getElementById('close-id');
+    closeBtn.classList.add('active-btn');
+    setTimeout(function timeFunc() {
+        closeBtn.classList.remove('active-btn');
+    }, 300);
+}
+
+function connectApiAPOD()
+{
+    async function fetchAPOD() {
+        const response = await fetch(addressAPOD);
+        const data = await response.json();
+        return data;
+    }
+
+    function ShowAPOD() {
+        let resultFromApi = fetchAPOD();
+        GenerateHtmlForResult(resultFromApi);
+    }
+
+    function GenerateHtmlForResult(resultFromApi) {
+        resultFromApi.then((result) => {
+            //console.log(result);
+            const {date, explanation, title, url} = result;
+            document.querySelector('.title-div-APOD').innerHTML = '<h2>' + title + '</h2>';
+            document.querySelector('.date-div-APOD').innerHTML = '<p>Date: ' + date + '</p>';
+            document.querySelector('.text-div-APOD').innerHTML = '<p>' + explanation + '</p>';
+            document.querySelector('.img-div-APOD').innerHTML = '<img src="' + url + '" alt="NASA API" style = "border-radius: 20px">';
+            let buttonCloseDiv = document.createElement('div');
+            buttonCloseDiv.className = 'div-button-close';
+            buttonCloseDiv.innerHTML = '<button type="button" id="close-id" class="test">CLOSE</button>';
+            document.querySelector('.modal-APOD-main').append(buttonCloseDiv);
+            runCloseBtnFunc();
+            }
+        )
+    }
+
+    ShowAPOD();
+}
+
+function connectApiPicsVideo()
+{
+
+    if (document.querySelector('.search-result') !== null) {
+        document.querySelector('.search-result').remove();
+    }
+    let searchResult = document.createElement('div');
+    searchResult.className = 'search-result';
+    document.querySelector('.main-page-div').append(searchResult);
+
+    let query = document.querySelector('.search-input-class').value;
+    async function fetchPics() {
+        const response = await fetch('' + addresOfPics + '' + query + '');
+        const data = await response.json();
+        return data;
+    }
+
+    function ShowPics() {
+
+                let resultFromApi = fetchPics();
+                GenerateHtmlForResult(resultFromApi);
+
+                function GenerateHtmlForResult(resultFromApi) {
+                    resultFromApi.then((result) => {
+                            //console.log(result);
+                        let videoType = document.getElementsByName('search-input')[0].dataset.typeOfSearch;
+                            for (let index = 1; index < result.collection.items.length; index++) {
+                                let mediatype = result.collection.items[index].data[0].media_type;
+                                let title = result.collection.items[index].data[0].title;
+                                if (mediatype === 'image' && videoType === 'video') {
+                                //console.log(result.collection.items[index].links[0].href);
+
+
+
+                                //console.log(mediatype);
+                                //console.log(picture);
+
+
+                                    async function fetchVideo() {
+                                        let pathVideoJson = result.collection.items[index].href;
+                                        const response = await fetch(pathVideoJson);
+                                        const data = await response.json();
+                                        return data;
+                                    }
+                                    function ShowVideo() {
+
+                                        let resultFromApi = fetchVideo();
+                                        GenerateHtmlForResult(resultFromApi);
+
+                                        function GenerateHtmlForResult(resultFromApi) {
+                                            resultFromApi.then((resultVideo) => {
+
+                                    let pathCurrentVideo = resultVideo[2];
+                                                //console.log(pathCurrentVideo);
+                                    let oneItemDiv = document.createElement('a');
+                                    oneItemDiv.className = 'one-item-main';
+                                    oneItemDiv.innerHTML = '<div class="pic-one-item"><video class="video-one-item" controls><source src="'+pathCurrentVideo+'" type="video/webm"></video></div>' +
+                                        '<div class="title-one-item"><h3>' + title + '</h3></div>';
+                                    // '<div class="description-one-item"><p>' + description + '</p></div>';
+                                    document.querySelector('.search-result').append(oneItemDiv);
+                                            })}
+                                }
+                                    ShowVideo()}
+                                else {
+                                    let picture = result.collection.items[index].links[0].href;
+                                    //console.log('img');
+                                let description = result.collection.items[index].data[0].description;
+
+                                let oneItemDiv = document.createElement('a');
+                                oneItemDiv.className = 'one-item-main';
+                                oneItemDiv.innerHTML = '<div class="pic-one-item"><img src="' + picture + '" alt="' + title + '" style="max-height: 700px; border-radius: 20px"></div>' +
+                                    '<div class="title-one-item"><h3>' + title + '</h3></div>';
+                                   // '<div class="description-one-item"><p>' + description + '</p></div>';
+                                document.querySelector('.search-result').append(oneItemDiv);
+                            }}
+
+                        }
+                    )
+                }
+            }
+
+            ShowPics();
+}
+
+function runCloseBtnFunc()
+{
+    document.querySelector('.div-button-close').addEventListener('click', function(event) {
+        if (event.target.matches('button')) {
+            event.preventDefault();
+            buttonClickEffectClose();
+            setTimeout(function timeFunc() {
+                if (document.querySelector('.search-result') !== null) {
+                    document.querySelector('.search-result').classList.remove('hidden-class');
+                }
+                allLinks.classList.remove('hidden-class');
+                centralDiv.classList.remove('hidden-class');
+                document.querySelector('.modal-APOD-main').remove();
+            }, 300);
+        }
+    });
+
+    document.querySelector('.div-button-close').addEventListener('touchstart', function(event) {
+        if (event.target.matches('button')) {
+            event.preventDefault();
+            buttonClickEffectClose();
+            setTimeout(function timeFunc() {
+                if (document.querySelector('.search-result') !== null) {
+                    document.querySelector('.search-result').classList.remove('hidden-class');
+                }
+                allLinks.classList.remove('hidden-class');
+                centralDiv.classList.remove('hidden-class');
+                document.querySelector('.modal-APOD-main').remove();
+            }, 300);
+        }
+    });
+}
+
+
+// window.history.pushState("object or string", "Title", "/new-url");
